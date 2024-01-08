36e5ecf1761eed93456834179af682e309d56554
marcovittoriozacca <zaccamarco01@gmail.com>
2024-01-08 15:45:52 +0100

    feat: complete style + ticket generator

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,26 +1,58 @@
 const kmXhourPrice = 0.21;
+let price;
 //Campi di input
 const nomePsg = document.getElementById('name');
 const kmDaPercorrere = document.getElementById('km');
 const etaPsg = document.getElementById('eta');
-let price;
+
+//campi del biglietto generato
+const nomePsgBiglietto = document.getElementById('nomeBiglietto');
+const offertaBiglietto = document.getElementById('offerta');
+const costoBigliettoHTML = document.getElementById('costoBiglietto');
+const carrozzaHTML = document.getElementById('carrozza');
+const codiceCPHTML = document.getElementById('codiceCP');
+const myTicketHTML = document.getElementById('myTicket');
+
 
 //Bottoni
 const generaBigliettoHTML = document.getElementById('generaBiglietto');
 const formattaFormHTML = document.getElementById('formattaForm');
 
 generaBigliettoHTML.addEventListener("click", function(){
-    
+
+    myTicketHTML.className = 'd-block'
+
+    nomePsgBiglietto.innerHTML = nomePsg.value.toUpperCase();
     price = kmDaPercorrere.value * kmXhourPrice;
-    console.log(nomePsg.value);
 
     if(etaPsg.value == "minorenne"){
         price = price - ( price * 0.2 );
-        console.log(`Il prezzo del biglietto è di ${price.toFixed(2)}€. è stato applicato uno sconto del 20%`);
+        offertaBiglietto.innerHTML = 'Sconto del 20%'
+        costoBigliettoHTML.innerHTML = `${price.toFixed(2)}€`;
+        carrozzaHTML.innerHTML =  randomNumGen(1, 10);
+        codiceCPHTML.innerHTML =  randomNumGen(1000, 9000);
+        
+
     }else if (etaPsg.value == "over65"){
         price = price - ( price * 0.4 );
-        console.log(`Il prezzo del biglietto è di ${price.toFixed(2)}€. è stato applicato uno sconto del 40%`);
+        offertaBiglietto.innerHTML = 'Sconto del 40%'
+        costoBigliettoHTML.innerHTML = `${price.toFixed(2)}€`;
+        carrozzaHTML.innerHTML =  randomNumGen(1, 10);
+        codiceCPHTML.innerHTML =  randomNumGen(1000, 9000);
     } else {
-        console.log(`Il prezzo del biglietto è di ${price.toFixed(2)}€.`);
+        offertaBiglietto.innerHTML = 'Biglietto Standard'
+        costoBigliettoHTML.innerHTML = `${price.toFixed(2)}€`;
+        carrozzaHTML.innerHTML =  randomNumGen(1, 10);
+        codiceCPHTML.innerHTML =  randomNumGen(1000, 9000);
     }
+})
+
+function randomNumGen(min, max){
+    return Math.floor (Math.random()*(max-min + 1)+min)
+}
+
+formattaFormHTML.addEventListener("click", function(){
+    nomePsg.value = null;
+    kmDaPercorrere.value = null;
+    etaPsg.value = "minorenne";
 })
